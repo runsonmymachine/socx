14814e2e96ac60c37674a189e9571d85b620cae2
Sagi Kimhi <sagi.kimhi@wiliot.com>
2025-02-08 13:12:40 +0200

    feat(plugins): add documentation to command example plugins
    Signed-off-by: Sagi Kimhi <sagi.kimhi@wiliot.com>

--- src/plugins/cli/roy.py
+++ src/plugins/cli/roy.py
@@ -2,4 +2,5 @@ import click
 
 @click.command()
 def cli():
+    """Command example plugin invoked by calling `covgen roy`."""
     print("Hello from Roy")
--- src/plugins/cli/test.py
+++ src/plugins/cli/test.py
@@ -3,4 +3,5 @@ import click as click
 
 @click.command()
 def cli():
+    """Command example plugin."""
     click.secho("Hello from test plugin!", color="purple", underline=True)
