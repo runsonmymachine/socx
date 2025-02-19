5388209b3b2d1b8af079cdad8d13834f4f99e564
Sagi Kimhi <sagi.kim5@gmail.com>
2025-02-19 17:06:52 +0200

    chore(core): export PtrMixin from mixins
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/core/__init__.py
+++ src/socx/core/__init__.py
@@ -1,2 +1,8 @@
-from .mixins import TypedPtrMixin as TypedPtrMixin
+__all__ = (
+    "UIDMixin",
+    "PtrMixin",
+    "TypedPtrMixin",
+)
 from .mixins import UIDMixin as UIDMixin
+from .mixins import TypedPtrMixin as PtrMixin
+from .mixins import TypedPtrMixin as TypedPtrMixin
