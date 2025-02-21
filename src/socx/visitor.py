356fdcdc8122c7db08e1bc3448a7a4f0f98fe77e
Sagi Kimhi <sagi.kim5@gmail.com>
2025-02-21 11:56:01 +0200

    fix(visitor): add empty __slots__ to protocol classes
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx/visitor.py
+++ src/socx/visitor.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Protocol
+from collections.abc import Iterable
 
 
 __all__ = (
@@ -16,51 +17,65 @@ __all__ = (
 
 
 class Node[NODE](Protocol):
-    def accept(self, v: Visitor[NODE]) -> None:
+    __slots__ = ()
+
+    def accept(self, v: Visitor[Node]) -> None:
         """Accept a visit from a visitor."""
         ...
 
 
 class Visitor[NODE](Protocol):
+    __slots__ = ()
+
     def visit(self, n: NODE) -> None:
         """Visit a node."""
         ...
 
 
-class Structure[NODE](Protocol):
+class Structure[NODE](Iterable[Node]):
+    __slots__ = ()
+
+    def accept(self, v: Visitor[NODE]) -> None:
+        """Accept a visit from a visitor."""
+        ...
+
     def children(self) -> list[NODE]:
         """Retrieve the immediate child nodes of a structure."""
         ...
 
 
 class Proxy[NODE](Protocol):
+    __slots__ = ()
+
     def children(self, n: NODE) -> list[NODE]:
         """Retrieve the immediate children of a node in a structure."""
         ...
 
 
 class Adapter[NODE](Protocol):
+    __slots__ = ()
+
     def accept(self, n: NODE, v: Visitor[NODE], p: Proxy[NODE]) -> None:
         """Adapt a visitor to visit a structure of nodes through a proxy."""
         ...
 
 
-class TopDownTraversal[NODE](Adapter):
-    def accept(self, n: NODE, v: Visitor[NODE], p: Proxy[NODE]) -> None:
+class TopDownTraversal(Adapter[Node]):
+    def accept(self, n: Node, v: Visitor[Node], p: Proxy[Node]) -> None:
         n.accept(v)
         for c in p.children(v):
             self.accept(c, v, p)
 
 
-class BottomUpTraversal[NODE](Adapter):
-    def accept(self, n: NODE, v: Visitor[NODE], p: Proxy[NODE]) -> None:
+class BottomUpTraversal[Node](Adapter[Node]):
+    def accept(self, n: Node, v: Visitor[Node], p: Proxy[Node]) -> None:
         for c in p.children(v):
             self.accept(c, v, p)
         v.visit(n)
 
 
-class ByLevelTraversal[NODE](Adapter[NODE]):
-    def accept(self, n: NODE, v: Visitor[NODE], p: Proxy[NODE]) -> None:
+class ByLevelTraversal[Node](Adapter[Node]):
+    def accept(self, n: Node, v: Visitor[Node], p: Proxy[Node]) -> None:
         q = [n]
         while q:
             t = []
