ae904125988f6b368c75c4d612cdcbf88d36d461
Sagi Kimhi <sagi.kim5@gmail.com>
2025-02-22 06:38:46 +0200

    chore(regression): fine tune configurations
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

--- src/socx_plugins/regression/rgr.py
+++ src/socx_plugins/regression/rgr.py
@@ -1,6 +1,7 @@
-import click
-
+import itertools as it
 from pathlib import Path
+
+import click
 
 from socx import Test
 from socx import console
@@ -8,8 +9,8 @@ from socx import settings
 
 
 def rerun_fail_hist():
-    failure_history = settings.regression.files.failure_history
-    history_fp = Path(failure_history.parent) / failure_history.name
+    failure_history = settings.regression.logs.files.failure_history
+    history_fp = Path(failure_history.directory) / failure_history.filename
     with click.open_file(history_fp, mode="r", encoding="utf-8") as file:
         for line in file:
             console.print(Test(line))
